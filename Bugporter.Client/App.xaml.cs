e7fd52d0648c66dea8bbda3a48531613cfee7218
SingletonSean <singletonsean4@gmail.com>
2022-11-28 19:09:31 -0500

    BGP-17: Create MAUI client project

---
+++ Bugporter.Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Bugporter.Client
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Bugporter.Client/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bugporter.Client
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
