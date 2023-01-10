54ee96d6ad096b4e6b78e922a98b41b38750a63c
SingletonSean <singletonsean4@gmail.com>
2023-01-09 19:52:46 -0500

    BGP-25: Connect report bug form to API

---
+++ Bugporter.Client/Features/ReportBug/API/IReportBugApiCommand.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugporter.Client.Features.ReportBug.API
+{
+    public interface IReportBugApiCommand
+    {
+        [Post("/bugs")]
+        Task<ReportBugResponse> Execute([Body(buffered: true)] ReportBugRequest bug);
+    }
+}
--- Bugporter.Client/Features/ReportBug/ReportBugCommand.cs
+++ Bugporter.Client/Features/ReportBug/ReportBugCommand.cs
@@ -1,4 +1,5 @@
-﻿using Bugporter.Client.Shared.Commands;
+﻿using Bugporter.Client.Features.ReportBug.API;
+using Bugporter.Client.Shared.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,32 @@ namespace Bugporter.Client.Features.ReportBug
     public class ReportBugCommand : AsyncCommandBase
     {
         private readonly ReportBugFormViewModel _viewModel;
+        private readonly IReportBugApiCommand _reportBugApiCommand;
 
-        public ReportBugCommand(ReportBugFormViewModel viewModel)
+        public ReportBugCommand(ReportBugFormViewModel viewModel, IReportBugApiCommand reportBugApiCommand)
         {
             _viewModel = viewModel;
+            _reportBugApiCommand = reportBugApiCommand;
         }
 
         protected override async Task ExecuteAsync(object parameter)
         {
-            
+            ReportBugRequest request = new ReportBugRequest()
+            {
+                Summary = _viewModel.Summary,
+                Description = _viewModel.Description,
+            };
+
+            try
+            {
+                ReportBugResponse response = await _reportBugApiCommand.Execute(request);
+
+                await Application.Current.MainPage.DisplayAlert("Success", $"Successfully reported bug #{response.Id}!", "Ok");
+            }
+            catch (Exception)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Failed to report bug.", "Ok");
+            }
         }
     }
 }
