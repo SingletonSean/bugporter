4afcf88698ad7e3e7d8af5ff19741a9fb5231bdb
SingletonSean <singletonsean4@gmail.com>
2023-07-24 19:00:01 -0400

    BGP-35: Load appsettings JSON file

--- Bugporter.Client/MauiProgram.cs
+++ Bugporter.Client/MauiProgram.cs
@@ -8,7 +8,9 @@ using Bugporter.Client.Pages.SignIn;
 using Bugporter.Client.Pages.SignUp;
 using Firebase.Auth;
 using Firebase.Auth.Providers;
+using Microsoft.Extensions.Configuration;
 using Refit;
+using System.Reflection;
 using System.Text.Json;
 
 namespace Bugporter.Client
@@ -26,10 +28,14 @@ namespace Bugporter.Client
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.AddAppSettings();
+
             builder.Services.AddSingleton<CurrentUserAuthHttpMessageHandler>();
+
+            string bugporterApiBaseUrl = builder.Configuration.GetValue<string>("BUGPORTER_API_BASE_URL");
             builder.Services
                 .AddRefitClient<IReportBugApiCommand>()
-                .ConfigureHttpClient(c => c.BaseAddress = new Uri("http://localhost:7071/api"))
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(bugporterApiBaseUrl))
                 .AddHttpMessageHandler<CurrentUserAuthHttpMessageHandler>();
 
             builder.Services.AddTransient<ReportBugViewModel>();
@@ -47,10 +53,12 @@ namespace Bugporter.Client
             builder.Services.AddTransient<SignUpView>(
                 s => new SignUpView(s.GetRequiredService<SignUpViewModel>()));
 
+            string firebaseApiKey = builder.Configuration.GetValue<string>("FIREBASE_API_KEY");
+            string firebaseAuthDomain = builder.Configuration.GetValue<string>("FIREBASE_AUTH_DOMAIN");
             builder.Services.AddSingleton(new FirebaseAuthClient(new FirebaseAuthConfig()
             {
-                ApiKey = "AIzaSyCU9W14p8vLLdlLikODcGw-xQl4h6csHPo",
-                AuthDomain = "bugporter-739c9.firebaseapp.com",
+                ApiKey = firebaseApiKey,
+                AuthDomain = firebaseAuthDomain,
                 Providers = new FirebaseAuthProvider[]
                 {
                     new EmailProvider()
@@ -59,6 +67,21 @@ namespace Bugporter.Client
             builder.Services.AddSingleton<CurrentUserStore>();
 
             return builder.Build();
+        }
+
+        private static void AddAppSettings(this MauiAppBuilder builder)
+        {
+            using Stream stream = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream("Bugporter.Client.appsettings.json");
+
+            if (stream != null)
+            {
+                IConfigurationRoot config = new ConfigurationBuilder()
+                    .AddJsonStream(stream)
+                    .Build();
+                builder.Configuration.AddConfiguration(config);
+            }
         }
     }
 }
